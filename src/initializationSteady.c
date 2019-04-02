52fe09925d0e58d3f317a2292926d967d7fa6493
Trushant Patel <trushant@ufl.edu>
2019-04-02 15:17:25 -0400

    Modified the initialization file to read the BC from input file for homogeneous and non-homogeneous Neumann condition. Doesn't work with Mixed condition though.

--- src/initializationSteady.c
+++ src/initializationSteady.c
@@ -24,14 +24,14 @@ int initializationSteady()
  {
   if ((face[i].bc == 1001 || face[i].bc == 1002 || face[i].bc == 1003 || face[i].bc == 1004 ) && face_flag[face[i].bc-1000] == 0)
   {
-      for (j=1;j<=n;j++)
-      {
-          if (face[i].bc == bc_number[j])
-          {
-              t[face[i].bc-1000] = bc_prop[j];
-              break;
-          }
-      }
+    for (j=1;j<=n;j++)
+    {
+        if (face[i].bc == bc_number[j])
+        {
+            t[face[i].bc-1000] = bc_prop[j];
+            break;
+        }
+    }
    printf("Dirichlet condition for wall %d is %lf K.\n",face[i].bc-1000,t[face[i].bc-1000]);
    face_flag[face[i].bc-1000] = 1;
   }
@@ -45,9 +45,17 @@ int initializationSteady()
 
   if ((face[i].bc == 3001 || face[i].bc == 3002 || face[i].bc == 3003 || face[i].bc == 3004 ) && face_flag[face[i].bc-3000] == 0)
   {
-   printf("Non-Homogeneous Neumann Condition for wall %d:\n",face[i].bc-3000);
-   printf("Enter the value of heat flux:");
-   scanf("%lf",&hf[face[i].bc-3000]);
+    for (j=1;j<=n;j++)
+    {
+        if (face[i].bc == bc_number[j])
+        {
+            hf[face[i].bc-1000] = bc_prop[j];
+            break;
+        }
+    }
+   printf("Non-Homogeneous Neumann Condition for wall %d is %lf.\n",face[i].bc-3000,hf[face[i].bc-3000]);
+//    printf("Enter the value of heat flux:");
+//    scanf("%lf",&hf[face[i].bc-3000]);
    face_flag[face[i].bc-3000] = 1;
   }
 
